9cbfd95fd339472f973c4e02550c4d5c88942ee6
jyun <jyunakaike@gmail.com>
2021-12-16 00:29:44 -0400

    grid rows, colums finance

--- src/pages/Finance.jsx
+++ src/pages/Finance.jsx
@@ -4,7 +4,16 @@ import '@styles/Finance.scss'
 const Finance = () => {
     return (
         <div className='finance-container'>
-            <div className='financeList'>
+            <div className='divTrial'>
+                <div className='total'>
+                    <div className='title'>
+                        <h1>total</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>Tabla </h2>
+                    </div>
+                </div>
                 <div className='gain' >
                     <div className='title'>
                         <h1>Gains</h1>
@@ -13,8 +22,6 @@ const Finance = () => {
                     <div className='detail'>
                         <h2>Tabla </h2>
                     </div>
-
-
                 </div>
                 <div className='spend' >
                     <div className='title'>
@@ -34,12 +41,81 @@ const Finance = () => {
                         <h2>monton de contenidos</h2>
                     </div>
                 </div>
+
+                <div className='gain2' >
+                    <div className='title'>
+                        <h1>Gains</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>Tabla </h2>
+                    </div>
+                </div>
+                <div className='spend2' >
+                    <div className='title'>
+                        <h1>Spends </h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>tabla</h2>
+                    </div>
+                </div>
+                <div className='savingAccount2' >
+                    <div className='title'>
+                        <h1>Saving Account</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>monton de contenidos</h2>
+                    </div>
+                </div>
+              
+                
             </div>
-            <div className='financeHistorial' >
-                <div className='historialBox'>
-                    un historial
+
+
+
+            {/* <div className='financeHistorial' >
+                <div className='total'>
+                    <div className='title'>
+                        <h1>total</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>Tabla </h2>
+                    </div>
                 </div>
             </div>
+            <div className='financeList'>
+                <div className='gain' >
+                    <div className='title'>
+                        <h1>Gains</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>Tabla </h2>
+                    </div>
+                </div>
+                <div className='spend' >
+                    <div className='title'>
+                        <h1>Spends </h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>tabla</h2>
+                    </div>
+                </div>
+                <div className='savingAccount' >
+                    <div className='title'>
+                        <h1>Saving Account</h1>
+                        <h1>***</h1>
+                    </div>
+                    <div className='detail'>
+                        <h2>monton de contenidos</h2>
+                    </div>
+                </div>
+            </div> */}
+
         </div>
     )
 }
