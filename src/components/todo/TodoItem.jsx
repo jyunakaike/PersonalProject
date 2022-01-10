77b32c29264565093c8f39937a3c65c85e37bb6c
jyun <jyunakaike@gmail.com>
2022-01-10 12:50:20 -0400

    Todo TodoList component

---
+++ src/components/todo/TodoItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TodoItem = () => {
+    return (
+        <div>
+            Task numero 1
+        </div>
+    )
+}
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import { TodoItem } from './TodoItem'
+
 export const TodoList = () => {
     return (
         <section className='TodoList-Container'>
-            Esto es la lista de todoList
+            <header className='Todo-header'>
+                <h1>My Day</h1>
+            </header>
+            <main className='Todo-main'>
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+                <TodoItem />
+            </main>
+
+            <footer className='Todo-footer'>
+                <input type="text" className='input-addTodo' name='Input-addTodo' placeholder='Add Task' />
+            </footer>
+
         </section>
     )
 }
